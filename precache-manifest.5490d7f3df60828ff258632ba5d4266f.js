602709ddd23b98f8fc743767efa60ca6e5d38847
Chanchana Wicha <chanchana@wongnai.com>
2020-10-26 19:12:43 +0700

    Updates

---
+++ precache-manifest.5490d7f3df60828ff258632ba5d4266f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f159f8f9e4420d021891741e328b173",
+    "url": "/kinrai/index.html"
+  },
+  {
+    "revision": "3b825a8119947f2f0cf2",
+    "url": "/kinrai/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fe34c8755804e5c88788",
+    "url": "/kinrai/static/js/2.de74c0be.chunk.js"
+  },
+  {
+    "revision": "a07009ca127e1234d8db0e2f6bed29f6",
+    "url": "/kinrai/static/js/2.de74c0be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b825a8119947f2f0cf2",
+    "url": "/kinrai/static/js/main.e0af475d.chunk.js"
+  },
+  {
+    "revision": "8ab74a7877fab7835b61",
+    "url": "/kinrai/static/js/runtime-main.ddb58f0c.js"
+  }
+]);
